ffbee058c568953577efd49aecd9ad01eeb14d58
Veronika <vera_mij_xxx@mail.ru>
2024-11-09 00:36:41 +1000

    fix app height

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -7,21 +7,39 @@ import Projects from "./pages/ProjectsPage.tsx";
 import Skills from "./pages/SkillsPage.tsx";
 import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
 import Footer from "./components/Footer.tsx";
+import styled from "styled-components";
+
+
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
+
+const MainContent = styled.main`
+  flex: 1; 
+  display: flex;
+  flex-direction: column;
+`;
 
 const App: React.FC = () => {
   return (
     <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/skills" element={<Skills />} />
-      </Routes>
-       <Footer />
+      <AppContainer>
+        <Header />
+        <MainContent>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/skills" element={<Skills />} />
+          </Routes>
+        </MainContent>
+        <Footer />
+      </AppContainer>
     </Router>
   );
 };
 
-export default App
+export default App;
